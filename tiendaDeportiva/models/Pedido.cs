fa49f0dd8f5bbb1a2f5f2ba0ff8f9578c70ce864
Alejandro <gonzalez-a26@hotmail.com>
2026-04-16 19:02:15 -0600

    Clases

---
+++ tiendaDeportiva/models/Pedido.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace tiendaDeportiva.Models
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+
+        public DateTime Fecha { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public string IdUsuario { get; set; }
+
+        public decimal MontoTotal { get; set; }
+
+        public EstadoPedido Estado { get; set; } = EstadoPedido.Pendiente;
+
+        public List<DetallePedido> Detalles { get; set; } = new();
+    }
+}
