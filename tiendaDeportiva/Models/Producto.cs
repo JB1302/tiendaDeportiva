56a46c0ae52c1812fdf64885e535e8dc6a8479e0
JB1302 <jstevenbarrantes@gmail.com>
2026-04-16 19:05:34 -0600

    Helpers

---
+++ tiendaDeportiva/Models/Producto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace tiendaDeportiva.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Nombre")]
+        public string Nombre { get; set; }
+
+        [StringLength(100)]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+
+        public decimal Precio { get; set; } = decimal.Zero;
+
+
+
+    }
+}
