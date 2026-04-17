b77145022dacb143c9bd97de2f058fb35229a508
Alejandro <109242182+AleGonza2611@users.noreply.github.com>
2026-04-16 19:07:46 -0600

    Modelos

---
+++ tiendaDeportiva/models/Enum/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tiendaDeportiva.Models.Enum
+{
+    public enum Categoria
+    {
+        Futbol = 1,
+        Basquetbol = 2,
+        Natacion = 3,
+        Tenis = 4
+    }
+}
---
+++ tiendaDeportiva/models/Enum/EstadoPedido.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tiendaDeportiva.Models.Enum
+{
+    public enum EstadoPedido
+    {
+        Pendiente = 0,
+        Completado = 1
+    }
+}
