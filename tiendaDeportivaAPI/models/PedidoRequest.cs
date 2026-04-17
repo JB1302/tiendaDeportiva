95b2f84e080c08bda1718cd9991a403100be2d1d
Alejandro <109242182+AleGonza2611@users.noreply.github.com>
2026-04-16 21:10:37 -0600

    Add files via upload

---
+++ tiendaDeportivaAPI/models/PedidoRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tiendaDeportivaAPI.Models
+{
+    public class PedidoRequest
+    {
+        public List<PedidoItemRequest> Items { get; set; }
+    }
+}
