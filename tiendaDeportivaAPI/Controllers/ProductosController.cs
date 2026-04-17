2d8e594de6c63a86245018c01f655158e736ec28
Alejandro <109242182+AleGonza2611@users.noreply.github.com>
2026-04-16 21:59:06 -0600

    Update ProductosController.cs

--- tiendaDeportivaAPI/Controllers/ProductosController.cs
+++ tiendaDeportivaAPI/Controllers/ProductosController.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using tiendaDeportiva.Controllers;
 using tiendaDeportiva.Models;
+using tiendaDeportivaAPI.Models;
 
 namespace Productos.API.Controllers
 {
